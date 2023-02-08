5c9aa63d2e2d7c93bd1c5e9a6b5d0c2b241d4929
bhadawi404 <abadawi.911@gmail.com>
2023-02-08 19:56:43 +0700

    sales order

--- sales_order_smt/__manifest__.py
+++ sales_order_smt/__manifest__.py
@@ -9,9 +9,10 @@
     'description': "",
     'website': 'https://smtek-tool.com/',
     'depends': [
-        'core_smt','crm_smt'
+        'core_smt','crm_smt','master_data'
     ],
     'data': [
+        'views/sales_order_views.xml',
         'views/quotation_views.xml',
         'security/ir.model.access.csv',
         'views/quote_menu_views.xml',
